bd85e3d79c260a901771c4ca780f35417d399fa5
davidnbr <davidnbr_98@hotmail.com>
2023-06-20 11:35:06 -0500

    Create length_string_high_level.cpp

---
+++ Basics/length_string_high_level.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+//Ejemplo cadena de caracteres de bajo nivel
+int main()
+{
+    //Cadena de caracteres ejemplo: nombreDeUsuario
+    string miCadenaDeAltoNivel;
+    
+    miCadenaDeAltoNivel = "HolaMundo";
+
+    int longitud = miCadenaDeAltoNivel.length();
+
+    cout << longitud;
+
+    return 0;
+}
